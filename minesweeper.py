37ac34c1899e984a7c599aad663badbebb071b40
Caleb Kathman <38675383+RockerCaleb1234@users.noreply.github.com>
2018-04-28 19:39:23 -0400

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -9,107 +9,27 @@ import pygame
 from pygame.locals import *  # Preferably don't wildcard import
 
 from cell import Cell
-
-
-pygame.font.init()
-
-if not pygame.font:
-    print("Warning, fonts disabled")
-else:
-    myfont = pygame.font.SysFont("Comic Sans MS", 30)
-if not pygame.mixer:
-    print("Warning, sound disabled")
+from grid import Grid
 
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800, 600))
-
-pygame.display.set_caption("Minesweeper")
-
 rows = 9
 cols = 9
 w = 50
-GRAY = (127, 127, 127)  # Unused?
 bombImg = pygame.image.load("bomb.jpg")
 bombImg = pygame.transform.scale(bombImg, (w, w))
 
 endGame = False
 
-
-def make2DArray(rows, cols) -> list:
-    """
-    Generate a 2D list of safe and bomb cells.
-
-    Arguments:
-    rows - Number of rows.
-    cols - Number of columns.
-
-    Returns:
-    List of randomized cells.
-    """
-    mainArray = [[None for i in range(cols)] for j in range(rows)]
-
-    for i in range(len(mainArray)):
-        for j in range(len(mainArray[i])):
-            mainArray[i][j] = Cell(i, j, w)
-            ri = randint(0, 4)
-            # print(ri)
-            if ri == 1:
-                mainArray[i][j].bomb = True
-    return mainArray
-# def funct():
-#     i = 0
-#     j = 0
-#     mainArray = []
-#     arr = []
-#     for i in range(rows):
-#         for j in range(cols):
-#             x = Cell(i,j,w)
-#             x.cellID = (i,j)
-#             arr.insert(j, x)
-#             ri = randint(0,4)
-#             #print(ri)
-#             if ri == 1:
-#                 x.bomb = True
-#         mainArray.insert(i, arr)
-#     return mainArray
-
-
-def drawGrid(grid) -> None:
-    """
-    Draw the minesweeper grid.
-
-    Arguments:
-    grid - 2D list of cells.
-
-    Returns:
-    None.
-    """
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            cell = grid[i][j]
-            pygame.draw.rect(gameDisplay, cell.color, [
-                             cell.x, cell.y, cell.w-1, cell.w-1])
-            if cell.revealed == True:
-                if cell.bomb == True:
-                    gameDisplay.blit(bombImg, (cell.x, cell.y))
-                if cell.bomb == False:
-                    if(cell.touching > 0):
-                        gameDisplay.blit(myfont.render(str(cell.touching),
-                                                       True,
-                                                       (255, 0, 0)),
-                                         (cell.x+15, cell.y))
-
-
 # Generate grid
-grid = make2DArray(rows, cols)
+grid = Grid(rows,cols)
 # Count adjacent bombs
-for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        grid[i][j].getTouching(grid)
+for i in range(len(grid.array)):
+    for j in range(len(grid.array[i])):
+        grid.array[i][j].getTouching(grid.array)
 
-drawGrid(grid)
+grid.drawGrid()
 
 while not endGame:
     for event in pygame.event.get():
@@ -120,23 +40,14 @@ while not endGame:
         if event.type == pygame.MOUSEBUTTONUP:
             bomb = False
             # print(event.pos)
-            for i in range(len(grid)):
-                for j in range(len(grid[i])):
-                    cell = grid[i][j]
+            for i in range(len(grid.array)):
+                for j in range(len(grid.array[i])):
+                    cell = grid.array[i][j]
                     if event.pos[0] > cell.x \
                         and event.pos[0] < cell.x + w \
                         and event.pos[1] > cell.y \
                         and event.pos[1] < cell.y + w:
-                        cell.color = (90, 90, 90)
-                        cell.revealed = True
-                        drawGrid(grid)
-                        if cell.bomb == True:
-                            bomb = True
-                        else:
-                            print("Touching: ", cell.touching)
-                            if cell.touching == 0: #NOT TESTED
-                                cell.showSurrounding(grid) #NOT TESTED
-            print("Bomb clicked: ", bomb)
+                        cell.action()
 
     pygame.display.update()
 
